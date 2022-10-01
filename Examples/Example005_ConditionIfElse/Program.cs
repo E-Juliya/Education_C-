3c20391e54f5bf62604407754b2d326ffa0b1796
JuliyaElatova <elatova_jul@mail.ru>
2022-10-01 21:51:13 +0500

    Изучили команды IF и ELSE

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя^ ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "juliya")
+{
+    Console.WriteLine("Ура, это же Juliya!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
