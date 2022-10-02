bf163c994946dfa2f8ec3f4126ca9dd7a6a4e373
JuliyaElatova <elatova_jul@mail.ru>
2022-10-02 18:36:29 +0500

    Сохранили свои труды

---
+++ Lesson002/Program.cs
@@ -0,0 +1,20 @@
+﻿void FindEven(int N)
+{
+    int index = 1;
+    while (index < N)
+    {
+        if (index % 2 == 0)
+        { Console.WriteLine(index); }
+        index++;
+    }
+
+}
+
+int GetN()
+{
+    Console.WriteLine("Ведите число N");
+    int N = Convert.ToInt16(Console.ReadLine());
+    return N;
+}
+int result = GetN();
+FindEven(result);
