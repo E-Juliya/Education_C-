13f4bcc4fbcec2fe95119d231ee110ea09bd0ec3
JuliyaElatova <elatova_jul@mail.ru>
2022-10-06 22:40:33 +0500

    Решили задачу № 2

--- HomeWork/HM_01/Program.cs
+++ HomeWork/HM_01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее: 
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a = -9;
+int b = -3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+int min = a;
+
+if (a < min) min = a;
+if (b < min) min = b;
+
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
