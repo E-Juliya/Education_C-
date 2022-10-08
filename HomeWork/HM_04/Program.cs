ab0cf32c1b4211ac486bdd3a06c6a34f79501f5a
JuliyaElatova <elatova_jul@mail.ru>
2022-10-08 16:28:19 +0500

    Решаем дз

--- HomeWork/HM_04/Program.cs
+++ HomeWork/HM_04/Program.cs
@@ -1,13 +1,36 @@
-﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа 
+// от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-int N = 3;
-index == 0;
+double GetValue();
+{
+    Console.Write("от 1 до N: ");
+    double value = Convert.ToDouble(Console.ReadLine());
+    return value;
+}
+bool IsEven(Double x);
 
-Console.WriteLine(N);
+{
+    if (x % 2 == 0) ;
+    return true;
+    else
+        return false;
+}
 
-if (N % 2 == 0);
-Console.WriteLine("a - четное");
+double next(double N);
+{
+    double x -1;
+    while (x < N) ;
+    {
+        if = (IsEven(x));
+        System.Console.WriteLine(x);
+        x += 1;
+    }
+    return x;
+}
+
+double N - GetValue();
+next(N);
 
