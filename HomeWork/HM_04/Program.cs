f5b688985e9a2be5f9e58428240e7cb6bcdcbfdc
JuliyaElatova <elatova_jul@mail.ru>
2022-10-11 20:22:06 +0500

    Решаем проблемы

--- HomeWork/HM_04/Program.cs
+++ HomeWork/HM_04/Program.cs
@@ -1,36 +1,14 @@
-﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа 
-// от 1 до N.
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все 
+чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
 
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
+int i;
+Console.Write("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (i = 1; i < N; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
 
-double GetValue();
-{
-    Console.Write("от 1 до N: ");
-    double value = Convert.ToDouble(Console.ReadLine());
-    return value;
-}
-bool IsEven(Double x);
-
-{
-    if (x % 2 == 0) ;
-    return true;
-    else
-        return false;
-}
-
-double next(double N);
-{
-    double x -1;
-    while (x < N) ;
-    {
-        if = (IsEven(x));
-        System.Console.WriteLine(x);
-        x += 1;
-    }
-    return x;
-}
-
-double N - GetValue();
-next(N);
 
