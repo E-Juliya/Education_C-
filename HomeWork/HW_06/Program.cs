983384cc7619cf01c07cfbc39e9005a7f42c204b
JuliyaElatova <elatova_jul@mail.ru>
2022-10-17 21:33:51 +0500

    Решили задачу

--- HomeWork/HW_06/Program.cs
+++ HomeWork/HW_06/Program.cs
@@ -1,64 +1,28 @@
 ﻿/* Задача 13: Напишите метод, который выводит третью цифру заданного числа 
-или сообщает, что третьей цифры нет.
+ или сообщает, что третьей цифры нет.
 645 -> 6
 78 -> третьей цифры нет
 32679 -> 6
+*/
 
-int Number(int number);
+
+Console.Write("Введите любое число: N = ");
+
+ThirdMethod(); ;
+
+void ThirdMethod()
 {
+    int N = Convert.ToInt32(Console.ReadLine());
+    int J;
+    if (N < 100)
+        Console.WriteLine("Третьей цифры числа нет");
     else
-{
-	if (number < 100)
-	    {
-	        Console.WriteLine("Третьей цифры в числе нет");
-	    }
-	    while (number > 100)
-	    {
-	        if (number > 999)
-	        {
-	            number = number / 10;
-	        }
-	        else
-	        {
-	            number = number % 10;
-	            Console.WriteLine($"Третья цифра числа = {number}");
-	        }
-	    }
-	    return number;
-}
-}
-//Console.Write("Введите число ");
-//int number = int.Parse(Console.ReadLine());
-
-static void task13(int num)
     {
-
-        if(num.ToString().Length>=3)
-        {
-            System.Console.WriteLine($" Третья цифра {num.ToString()[2]}");
-        }
-        else   Console.WriteLine("Третьего числа нет");
+        for (int i = 0; N > 1000; i++)
+        { N = N - 1000; }
+        for (J = 0; N > 100; J++)
+        { N = N - 100; }
+        Console.Write("Третья цифра введённого числа = ");
+        Console.Write(J);
     }
-
-    */
-    double GetValue(string text)
-{
-    Console.WriteLine(text + " ");
-    double value = Convert.ToDouble(Console.ReadLine());
-    return value;
 }
-
-
-
-double Index2(double N)
-{
-double a1 = (N % 100);
-double a2 = (N % 10);
-double a3 = (a1 - a2) / 10;
-return a3;
-}
-
-double N = GetValue("Ваше трёхзначное число?");
-Index2(N);
-
-Console.WriteLine("Вторая цифра " + Index2(N));
