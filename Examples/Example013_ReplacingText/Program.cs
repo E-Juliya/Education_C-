cfbbaa6d4f086c920f5f1d1ad57ab0ed981d8532
JuliyaElatova <elatova_jul@mail.ru>
2022-10-20 20:18:18 +0500

    Замена текста

---
+++ Examples/Example013_ReplacingText/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача: В тексте нужно все пробелы заменить ......
+
+string text = "_Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие русского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            012345
+// s[3] // r
+
+string Replece(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replece(text, ' ', '/');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replece(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
