b4b928d7d7a55d1c36f658556cdca7bd2a293d78
JuliyaElatova <elatova_jul@mail.ru>
2022-10-20 18:49:16 +0500

    Задачи с методами

---
+++ Examples/Example011_MassivMetod/Program.cs
@@ -0,0 +1,31 @@
+﻿
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; // создай новый массив в котором будет 10 элементов. По умолчанию будет заполнен нулями. 
+                           // напишем метод void 
+
+
+
+FillArray(array);
+PrintArray(array);
