c66e29a33c56c452689cce07a989852dd93c9cd1
JuliyaElatova <elatova_jul@mail.ru>
2022-10-31 21:45:34 +0500

    Решила задачу 41

---
+++ HomeWork/HW_15/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Введите числа через запятую: ");
+int[] M = StringToNum(Console.ReadLine());
+PrintArray(M);
+int s = 0;
+for (int i = 0; i < M.Length; i++)
+{
+    if (M[i] > 0)
+    {
+        s++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {s}");
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] M = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        M[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return M;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
